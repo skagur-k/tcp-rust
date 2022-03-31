b7281df5afac46ed30f74ed2caa64afbbf554588
skagur-k <skagur0329@gmail.com>
2022-04-01 00:51:13 +0900

    Added tcp.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use tun_tap;
 use std::io;
+use std::collections::HashMap;
+use std::net::Ipv4Addr;
+
+mod tcp;
+struct Quad{
+    src: (Ipv4Addr, u16),
+    dst: (Ipv4Addr, u16),
+}
 fn main() -> io::Result<()>{
+    let mut connections: HashMap<Quad, tcp::State> = Default::default();
+
     // Set up virtual network interface (tun0) with the tun mode
     // buf - buffer with size 1504: MTU(1500 octets) + 4 (flag, proto)
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
@@ -10,19 +20,37 @@ fn main() -> io::Result<()>{
 
     // loops and receives packets
     loop{
+        // Layer 3
         let nbytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
-
+        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
         
-        if proto != 0x0800 {
-            // If protocol # is not 0x0800 (IPv4), then ignore and continue
+        if eth_proto != 0x0800 {
+            // If Layer 3 protocol is not 0x0800 (IPv4), then ignore and continue
             continue;
         }
 
+        // Layer 4
         match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]){
             Ok(packet) => {
-                eprintln!("Read {} bytes [FLAGS: {}, PROTO: {:#06x}]: {:x?} ", nbytes - 4, flags, proto, packet.to_header());
+                let src = packet.source_addr();
+                let dst = packet.destination_addr();
+                let proto = packet.protocol();
+                let _payload_len = packet.payload_len();
+
+                if proto != 0x06{
+                    // If Layer 4 protocol is not 0x06 (TCP), then ignore and continue
+                    continue
+                }
+
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4+packet.slice().len()..]){
+                    Ok(p) => {
+                        eprintln!("SRC: {} -> DST: {} [{} bytes of TCP to Port: {}] ", src, dst, p.slice().len(), p.destination_port());
+                    },
+                    Err(e) => {
+                        eprintln!("Unexpected TCP Header: {:?}", e);
+                    }
+                }
             },
             Err(e) => {
                 eprintln!("Ignoring unexpected packet: {:?}", e);
